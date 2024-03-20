6dda7e701467f622d830c0042cf1ce8bcf72787c
Krishnakumarshanmugaraj <163254567+Krishnakumarshanmugaraj@users.noreply.github.com>
2024-03-20 21:08:49 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+var username= document.forms['forms']['username'];
+var password= document.forms['forms']['password'];
+function validated()
+{ if (username.value.lenght <9)
+    username.style.border
+    {
+        return false;
+    }
+
+}
